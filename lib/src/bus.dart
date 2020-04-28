bcd633e27f33d6a31b64a71924151f55daa78f2c
Plague Fox <plugfox@gmail.com>
2020-04-28 12:53:40 +0300

    static event bus controller

--- lib/src/bus.dart
+++ lib/src/bus.dart
@@ -1,84 +1,35 @@
 import 'dart:async';
 
 import 'exception.dart';
-import 'filters.dart';
 import 'transition.dart';
+
+/// TODO: реализовать возможность распределения по изолятам (при этом оставить поддержку веба)
+/// Менеджеры ответственные за общение между изолятами с основным потоком
 
 ///
 class EventBus {
-  final _EventBusState _state;
-
-  /// Последнее событие
-  Event get event => _state.lastEvent;
-
-  /// Поток событий
-  Stream<Event> events;
-
-  /// Поток ошибок
-  Stream<EventBusException> errors;
-
-  /// Добавить событие
-  void emit(Event nextEvent) => _state.add(nextEvent);
-
-  /// Закрыть шину данных
-  /// ВНИМАНИЕ, ЭТО НЕОБРАТИМО
-  Future<void> kill() => _state.close();
-
-  /// TODO: Добавить возможность подписки на темы
-
-  /// Поток фильтрованных событий
-  /// Укажите дженерик для фильтрации
-  Stream<Event> whereEvents<EventType extends Event>() =>
-      events.transform(WhereTypeTransformer<Event, EventType>());
-
-  /// Поток фильтрованных смен событий
-  /// onlyCompletely - возвращать только полную смену,
-  /// с определенным предыдущим событием
-  /// Укажите дженерики для фильтрации
-  Stream<Transition<PrevEvent, NextEvent>>
-      whereTransition<PrevEvent extends Event, NextEvent extends Event>(
-              {bool onlyCompletely = false}) =>
-          events.transform(
-              WhereTransitionTransformer<Event, PrevEvent, NextEvent>(
-                  onlyCompletely: onlyCompletely));
+  static final StreamController<Event> _controller =
+      StreamController<Event>.broadcast();
 
   /// Получить объект синглтона шины данных
   factory EventBus() => _instance;
-  static final EventBus _instance = EventBus._internalSingleton();
-  EventBus._internalSingleton() : _state = _EventBusState() {
-    events = _state.events;
-    errors = _state.errors;
-  }
-}
-
-class _EventBusState implements Sink<Event> {
-  final StreamController<Event> _controller =
-      StreamController<Event>.broadcast();
-
-  /// Последнее событие
-  Event lastEvent;
+  static const EventBus _instance = EventBus._internalSingleton();
+  const EventBus._internalSingleton();
 
   /// Поток событий
-  Stream<Event> events;
+  Stream<Event> get events =>
+    _controller.stream.handleError((Object _) => null);
 
   /// Поток ошибок
-  Stream<EventBusException> errors;
+  Stream<EventBusException> get errors => _controller.stream.transform<EventBusException>(
+      StreamTransformer<Event, EventBusException>.fromHandlers(
+        handleData: (Event _, EventSink<EventBusException> __) => null,
+        handleError: (Object error, StackTrace stackTrace, EventSink<EventBusException> sink) =>
+            sink.add(EventBusException(error: error, stackTrace: stackTrace)),        
+      ));
 
-  _EventBusState() {
-    /// Инициализация шины данных
-    events = _controller.stream.handleError((Object _) => null);
-    errors = _controller.stream.transform<EventBusException>(
-        StreamTransformer<Event, EventBusException>.fromHandlers(
-      handleData: (Event nextEvent, EventSink<EventBusException> _) =>
-          lastEvent = nextEvent,
-      handleError: (Object error, StackTrace stackTrace,
-              EventSink<EventBusException> sink) =>
-          sink.add(EventBusException(error: error, stackTrace: stackTrace)),
-    ));
-  }
-
-  @override
-  void add(Event nextEvent) {
+  /// Добавить событие
+  void emit(Event nextEvent) {
     try {
       _controller.add(nextEvent);
     } on dynamic catch (error, stackTrace) {
@@ -86,8 +37,9 @@ class _EventBusState implements Sink<Event> {
     }
   }
 
-  @override
-  Future<void> close() => _controller.close();
+  /// Закрыть шину данных
+  /// ВНИМАНИЕ, ЭТО НЕОБРАТИМО
+  Future<void> kill() => _controller.close();
 
   void _onError(Object error, StackTrace stackTrace) {
     _controller.addError(error, stackTrace);
